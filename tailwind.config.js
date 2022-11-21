00950709b2a5e3cd152a8e87847c0ef06f372925
susu3131 <anny81327@gmail.com>
2022-11-21 23:13:31 +0800

    header & footer 文字新增

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   theme: {
     //theme 下覆蓋原tailwind樣式
     container: {
+      center: true,
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
@@ -16,10 +17,11 @@ module.exports = {
       colors: {
         'primary': '#2488B2', //主色
         'secondary': '#EC7B7B', //副色
-        'star': '#FFC700',
-        'light': '#8A8888',
-        'dark': '#116D94',
-        'bg-color': '#EEF6FA',
+        'star': '#FFC700', //黃色
+        'light': '#8A8888', //淺色文字
+        'dark-blue': '#116D94',//深藍
+        'light-blue': '#116D94',//深藍
+        'bg-color': '#EEF6FA', 
       },
     },
   },
