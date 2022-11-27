f225eca3a28618114977d7986d6a92dc265187f0
susu3131 <anny81327@gmail.com>
2022-11-27 23:43:20 +0800

    Update 2022-11-27T15:43:18.177Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,19 +1,33 @@
 "use strict";
 
-console.log('Hello!'); //功能尚未開放
-// 問題 選取class 跳alert
+console.log('Hello!'); //功能尚未開放 
+// onclick="notOpen()" 加在html標籤
 
-var notOpen = document.querySelector(".notopen");
-var notOpenAll = document.querySelectorAll(".notopen");
-console.log(notOpen);
-console.log(notOpenAll);
-notOpen.addEventListener("click", function (e) {
+function notOpen() {
   alert("\u6B64\u529F\u80FD\u5C1A\u672A\u958B\u653E");
-}); //搜尋框>典籍切換搜尋選單
+} //搜尋框>典籍切換搜尋選單
+
 
 var searchClick = document.querySelector("#search-click");
 var searchShow = document.querySelector("#search-hidden");
-searchClick.addEventListener("click", function (e) {
-  searchShow.classList.toggle('lg:flex');
-});
+
+if (searchClick) {
+  searchClick.addEventListener("click", function (e) {
+    searchShow.classList.toggle('lg:flex');
+  });
+} // const axios = require("axios");
+// const options = {
+//   method: 'GET',
+//   url: 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/photos',
+//   params: {hotel_id: '363464'},
+//   headers: {
+//     'X-RapidAPI-Key': '8a129b9e36mshfc4b21874db1717p1d3d01jsn2fa5c0b45161',
+//     'X-RapidAPI-Host': 'hotels-com-provider.p.rapidapi.com'
+//   }
+// };
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
 //# sourceMappingURL=all.js.map
