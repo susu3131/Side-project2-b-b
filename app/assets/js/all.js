e76916f0ab7691081e0f793e09e22ac4a381f7e7
susu3131 <anny81327@gmail.com>
2022-11-27 18:04:26 +0800

    首頁toggle功能

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,15 +1,29 @@
 console.log('Hello!');
 
 //功能尚未開放
-
+// 問題 選取class 跳alert
 
 const notOpen = document.querySelector(".notopen")
+const notOpenAll = document.querySelectorAll(".notopen")
+
+console.log(notOpen)
+console.log(notOpenAll)
+
+notOpen.addEventListener("click",function(e){
+
+  alert(`此功能尚未開放`)
+})
 
 
-function notOpenClick() {
-  
-  notOpen.addEventListener("click", function (e) {
-    alert("此功能尚未開放")
 
-  })
-}
+//搜尋框>典籍切換搜尋選單
+
+const searchClick = document.querySelector("#search-click")
+const searchShow = document.querySelector("#search-hidden")
+
+
+searchClick.addEventListener("click",function(e){
+  searchShow.classList.toggle('lg:flex')
+})
+
+
