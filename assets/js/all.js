57b85ac90382075a6c179f55e24a7c377009506c
susu3131 <anny81327@gmail.com>
2022-11-27 16:00:12 +0800

    Update 2022-11-27T08:00:08.629Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,12 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
+console.log('Hello!'); //功能尚未開放
+
+var notOpen = document.querySelector(".notopen");
+
+function notOpenClick() {
+  notOpen.addEventListener("click", function (e) {
+    alert("此功能尚未開放");
+  });
+}
 //# sourceMappingURL=all.js.map
