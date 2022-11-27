3aa8d26ffb99c9a70852815e6e25c10e7daeca67
susu3131 <anny81327@gmail.com>
2022-11-27 18:04:47 +0800

    Update 2022-11-27T10:04:44.911Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,19 @@
 "use strict";
 
 console.log('Hello!'); //功能尚未開放
+// 問題 選取class 跳alert
 
-var notOpen = document.querySelector("notopen");
+var notOpen = document.querySelector(".notopen");
+var notOpenAll = document.querySelectorAll(".notopen");
+console.log(notOpen);
+console.log(notOpenAll);
+notOpen.addEventListener("click", function (e) {
+  alert("\u6B64\u529F\u80FD\u5C1A\u672A\u958B\u653E");
+}); //搜尋框>典籍切換搜尋選單
 
-function notOpenClick() {
-  notOpen.addEventListener("click", function (e) {
-    alert("此功能尚未開放");
-  });
-}
+var searchClick = document.querySelector("#search-click");
+var searchShow = document.querySelector("#search-hidden");
+searchClick.addEventListener("click", function (e) {
+  searchShow.classList.toggle('lg:flex');
+});
 //# sourceMappingURL=all.js.map
